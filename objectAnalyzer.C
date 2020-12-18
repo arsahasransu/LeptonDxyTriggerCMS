8cfdccaf4483f2787a84066cedcb89b419da46e3
A.R.Sahasransu <a.r.sahasransu@gmail.com>
2020-12-18 09:20:14 +0100

    skimmer added

--- objectAnalyzer.C
+++ objectAnalyzer.C
@@ -1,8 +1,8 @@
 int objectAnalyzer() {
 
   auto chain = new TChain("demo/TrigObjects");
-  chain->Add("../out_1.root");
-  //chain->Add("/pnfs/iihe/cms/store/user/asahasra/EphemeralHLTPhysics1/DisplacedLeptonHLT_EphemeralHLTSkim1_asahasra/201216_220732/0000/out_*.root");
+  //chain->Add("../out_1.root");
+  chain->Add("/pnfs/iihe/cms/store/user/asahasra/EphemeralHLTPhysics1/DisplacedLeptonHLT_EphemeralHLTSkim1_asahasra/201216_220732/0000/out_*.root");
 
   std::cout<<chain->GetEntries()<<std::endl;
 
@@ -42,7 +42,10 @@ int objectAnalyzer() {
   
   for(int event=0; event<chain->GetEntries(); event++) {
 
+    if(event%100000==1) std::cout<<event<<" entries processed"<<std::endl;
     chain->GetEntry(event);
+
+    if(ht_n==0) continue;
     
     hist_mu_n->Fill(mu_n);
     for(int muctr=0; muctr<mu_n; muctr++) {
