ebfeaf9abff869f6421452f861dcff653f46b6c9
A.R.Sahasransu <a.r.sahasransu@gmail.com>
2021-10-04 07:48:13 +0200

    Initial commit for the Double Muon trigger

--- MuMu/plotter.C
+++ MuMu/plotter.C
@@ -4,11 +4,11 @@
 #include <vector>
 #include "enhance_plotter.C"
 
-double datasf = 1.1/213;
-double sig3cmsf = 100.0/330;
-double sig30cmsf = 100.0/330;
-double sig3msf = 100.0/330;
-double sig1msf = 100.0/330;
+double datasf = 0.5/95; // Data rate - 0.5 Hz, 95 events from parent trigger selection
+double sig3cmsf = 1.0/35;
+double sig30cmsf = 1.0/28;
+double sig1msf = 1.0/19;
+double sig3msf = 1.0/7;
 TString cutdeets = "Cut details";
 TFile* datafile = TFile::Open("hists_efmrl_MuMu16DisplacedSkim1.root","READ");
 TFile* sig3cmfile = TFile::Open("hists_STHDM3cm_doublemu.root","READ");
@@ -16,10 +16,13 @@ TFile* sig30cmfile = TFile::Open("hists_STHDM30cm_doublemu.root","READ");
 TFile* sig1mfile = TFile::Open("hists_STHDM1m_doublemu.root","READ");
 TFile* sig3mfile = TFile::Open("hists_STHDM3m_doublemu.root","READ");
 
+TString seltext[2] = {"line1", "line2"};
+
 std::vector<int> coloropt{1, kGreen-9, kGreen-7, kGreen-3, kGreen+2};
-int comparemultihistnormalized(TString cutname, TString var, int xbinlow, int xbinhigh, int rebin=-1, bool logY=false, bool isMConly=false, bool overflow=false, float legPos[]=(float []){0.7,0.75,0.95,1}) {
+int comparemultihist(TString cutname, TString var, int xbinlow, int xbinhigh, int rebin=-1, bool logY=false, bool isMConly=false, bool overflow=false, float legPos[]=(float []){0.7,0.75,0.95,1}, bool normalized=false) {
 
   std::vector<TString> legNam;
+  if(!isMConly) legNam.push_back("2018 data");
   legNam.push_back("c#tau = 3 cm");
   legNam.push_back("c#tau = 30 cm");
   legNam.push_back("c#tau = 1 m");
@@ -44,7 +47,7 @@ int comparemultihistnormalized(TString cutname, TString var, int xbinlow, int xb
   allhists.push_back(sig3mhist);
 
   if(rebin==-1) rebin = 1;
-  xbinlow = xbinlow==-1?0:xbinlow;
+  xbinlow = xbinlow==-1?1:xbinlow;
   xbinhigh = xbinhigh==-1?(overflow?allhists[0]->GetNbinsX()+1:allhists[0]->GetNbinsX()):xbinhigh;
   xbinlow = xbinlow/rebin;
   xbinhigh = xbinhigh/rebin;
@@ -75,23 +78,177 @@ int comparemultihistnormalized(TString cutname, TString var, int xbinlow, int xb
 
   TCanvas* c1;
   c1 = new TCanvas();
-  //gStyle->SetOptStat(0);
-  //allhists[0]->DrawNormalized("hist e1");
-  //for(unsigned int histctr=1; histctr<allhists.size(); histctr++) {
-  //allhists[histctr]->DrawNormalized("hist e1 same");
-  //}
-  //c1->SetLogy(logY);
-  c1 = enhance_plotter(allhists, legNam, allhists[0]->GetXaxis()->GetTitle(),allhists[0]->GetYaxis()->GetTitle(),legPos,logY);
+  c1 = enhance_plotter(allhists, legNam, allhists[0]->GetXaxis()->GetTitle(),allhists[0]->GetYaxis()->GetTitle(),legPos,logY,normalized);
   c1->SaveAs("./dirplots/"+cutname+"/"+cutname+"_"+var+".png");
   
   return -1;
 }
 
+// Comparison of the type cross-check between two histogram - filled v hollow
+int crossChecktwohist(TFile* file, vector<TString> cutname, TString var, int xbinlow, int xbinhigh, int rebin=-1, bool logY=false, float legPos[]=(float []){0.7,0.75,0.95,1}) {
+
+  std::vector<TString> legNam;
+  legNam.push_back("L3#mu filter - DoubleMu33Displaced");
+  legNam.push_back("L3#mu filter, p_{T}>16, |#eta|<2.5, d_{0}>0.1mm ");
+
+  TH1F* histfilled = (TH1F*) file->Get(cutname[0]+"_"+var);
+  TH1F* histhollow = (TH1F*) file->Get(cutname[1]+"_"+var);
+
+  std::vector<TH1F*> allhists;
+  allhists.push_back(histfilled);
+  allhists.push_back(histhollow);
+
+  // Get the title from histogram title
+  TString xtitle = histfilled->GetTitle();
+
+  if(rebin==-1) rebin = 1;
+  xbinlow = xbinlow==-1?0:xbinlow;
+  xbinhigh = xbinhigh==-1?allhists[0]->GetNbinsX()+1:xbinhigh;
+  xbinlow = xbinlow/rebin;
+  xbinhigh = xbinhigh/rebin;
+
+  for(unsigned int histctr=0; histctr<allhists.size(); histctr++) {
+    if(rebin!=1) allhists[histctr]->Rebin(rebin);
+
+    // Make changes to sig and bkg to enable good basic plotting
+    double err = 0.0;
+    allhists[histctr]->SetBinContent(xbinlow,allhists[histctr]->IntegralAndError(0,xbinlow,err));
+    allhists[histctr]->SetBinError(xbinlow,err);
+    err = 0.0;
+    allhists[histctr]->SetBinContent(xbinhigh,allhists[histctr]->IntegralAndError(xbinhigh,allhists[histctr]->GetNbinsX()+1,err));
+    allhists[histctr]->SetBinError(xbinhigh,err);
+
+    allhists[histctr]->GetXaxis()->SetRange(xbinlow, xbinhigh);
+    allhists[histctr]->GetXaxis()->SetTitle(xtitle);
+    allhists[histctr]->GetYaxis()->SetTitle("number of events");
+    
+    allhists[histctr]->SetTitle("");
+  }
+
+  allhists[0]->SetFillColor(kBlue);
+  allhists[1]->SetLineColor(kRed);
+  
+  TCanvas* c1;
+  c1 = new TCanvas();
+  c1 = enhance_plotter(allhists, legNam, allhists[0]->GetXaxis()->GetTitle(),allhists[0]->GetYaxis()->GetTitle(),legPos,logY,false);
+  c1->SaveAs("./dirplots/"+cutname[0]+"_"+cutname[1]+"/"+cutname[0]+"_"+cutname[1]+"_"+var+".png");
+
+  return -1;
+}
+
+int makeratehist(TString cutname, TString var, int xbinlow, int xbinhigh, int rebin=-1, bool logY=false, bool overflow=false, float legPos[]=(float []){0.7,0.75,0.95,1}, float seltextpos[]=(float []){0.1,1}, float yrange[]=(float []){0.1,1}, float drawsignalline=-1.0) {
+
+  std::vector<TString> legNam;
+  legNam.push_back("2018 data");
+  legNam.push_back("c#tau = 3 cm");
+  legNam.push_back("c#tau = 30 cm");
+  legNam.push_back("c#tau = 1 m");
+  legNam.push_back("c#tau = 3 m");
+
+  TString histname = cutname+"_"+var;
+  TH1F* datahist;
+  datahist = (TH1F*) datafile->Get(histname);
+  datahist->Scale(datasf);
+  TH1F* sig3cmhist = (TH1F*) sig3cmfile->Get(histname);
+  sig3cmhist->Scale(sig3cmsf);
+  TH1F* sig30cmhist = (TH1F*) sig30cmfile->Get(histname);
+  sig30cmhist->Scale(sig30cmsf);
+  TH1F* sig1mhist = (TH1F*) sig1mfile->Get(histname);
+  sig1mhist->Scale(sig1msf);
+  TH1F* sig3mhist = (TH1F*) sig3mfile->Get(histname);
+  sig3mhist->Scale(sig3msf);
+
+  // Get the title from histogram title
+  TString xtitle = sig3cmhist->GetTitle();
+
+  std::vector<TH1F*> allhists;
+  std::vector<TH1F*> allratehists;
+  allhists.push_back(datahist);
+  allhists.push_back(sig3cmhist);
+  allhists.push_back(sig30cmhist);
+  allhists.push_back(sig1mhist);
+  allhists.push_back(sig3mhist);
+
+  double axisscale = 0.0;
+  for(unsigned int histctr=0; histctr<allhists.size(); histctr++) {
+
+    allratehists.push_back((TH1F*) allhists[histctr]->Clone());
+    for(unsigned int bincnt=0; bincnt<allhists[histctr]->GetNbinsX(); bincnt++) {
+      double err = 0;
+      allratehists[histctr]->SetBinContent(bincnt, allhists[histctr]->IntegralAndError(bincnt,allhists[histctr]->GetNbinsX()+1,err));
+      allratehists[histctr]->SetBinError(bincnt, err);
+    }
+    allratehists[histctr]->GetXaxis()->SetRange(xbinlow, xbinhigh);
+    allratehists[histctr]->GetXaxis()->SetTitle(xtitle);
+    allratehists[histctr]->GetYaxis()->SetTitle("rate");
+
+    allratehists[histctr]->SetTitle("");
+
+    allratehists[histctr]->SetLineWidth(2);
+    allratehists[histctr]->SetLineColor(coloropt[histctr]);
+    if(histctr==1) {
+      axisscale = allratehists[0]->GetBinContent(0)*0.8/allratehists[1]->GetBinContent(0);
+      allratehists[histctr]->Scale(axisscale);
+    }
+    if(histctr>1) {
+      allratehists[histctr]->Scale(axisscale);
+    }
+  }
+
+  TCanvas* c1;
+  c1 = new TCanvas();
+  c1 = enhance_plotter_rate(allratehists, legNam, allratehists[0]->GetXaxis()->GetTitle(),allratehists[0]->GetYaxis()->GetTitle(),legPos,yrange,logY,false);
+
+  TPad* pad = (TPad*) c1->FindObject("pad3");
+  pad->cd();
+  TGaxis *axis;
+  axis = new TGaxis(allratehists[0]->GetBinLowEdge(xbinhigh+1),yrange[0],allratehists[0]->GetBinLowEdge(xbinhigh+1),yrange[1],yrange[0]/axisscale,yrange[1]/axisscale,510,"-L");
+  axis->SetLineColor(coloropt[1]);
+  axis->SetLabelColor(coloropt[1]);
+  axis->SetLabelFont(132);
+  axis->SetLabelSize(0.06);
+  axis->SetLabelOffset(-0.035);
+  axis->Draw();
+
+  if(drawsignalline!=-1.0) {
+    TLine *signalline = new TLine(allratehists[0]->GetBinLowEdge(xbinlow),drawsignalline,allratehists[0]->GetBinLowEdge(xbinhigh+1),drawsignalline);
+    signalline->SetLineWidth(2);
+    signalline->SetLineColor(coloropt[1]);
+    signalline->SetLineStyle(9);
+    signalline->Draw();
+  }
+
+  TLatex sel;
+  sel.SetTextFont(132);
+  sel.SetTextSize(0.065);
+  sel.DrawLatex(seltextpos[0], seltextpos[1]+0.1*(yrange[1]-yrange[0]), seltext[0]);
+  sel.DrawLatex(seltextpos[0], seltextpos[1], seltext[1]);
+  
+  c1->SaveAs("./dirplots/"+cutname+"/"+cutname+"_"+var+"_ratehist.png");
+  
+  return -1;
+}
+
 int plotter() {
-  
-  comparemultihistnormalized("genmumu", "log10d0", 20, 80, -1, false, true, false, (float []){0.15,0.65,0.35,0.95});
-  comparemultihistnormalized("genmumu", "pt", 0, 60, -1, true, true, false, (float []){0.7,0.65,0.9,0.95});
-  comparemultihistnormalized("genmumu", "eta", 2, -1, 2, true, true, false, (float []){0.4,0.2,0.6,0.6});
-  
+
+  std::vector<int> coloroptgens{1, kRed-9, kRed-7, kRed-3, kRed+2};
+  coloropt = coloroptgens;
+  //comparemultihist("genmumu", "log10d0", 20, 80, -1, false, true, false, (float []){0.15,0.65,0.35,0.95}, true);
+  //comparemultihist("genmumu", "pt", 0, 60, -1, true, true, false, (float []){0.7,0.65,0.9,0.95}, true);
+  //comparemultihist("genmumu", "eta", 2, -1, 2, true, true, false, (float []){0.4,0.2,0.6,0.6}, true);
+
+  vector<TString> cuts{"filt33mu","filt20parsel33"};
+  //crossChecktwohist(datafile,cuts,"pt",20,500,10,true,(float []){0.3,0.65,0.5,0.95});
+  //crossChecktwohist(datafile,cuts,"eta",-1,-1,-1,false,(float []){0.3,0.65,0.5,0.95});
+
+  std::vector<int> coloroptrate{1, kRed+2, kRed-3, kRed-7, kRed-9};
+  coloropt = coloroptrate;
+  seltext[0] = "#mu p_{T}>33 GeV, |#eta|<2.5";
+  seltext[1] = "#mu d_{0}>0.01 cm, N#mu#geq2";
+  //makeratehist("filt20parsel33", "pt2", 30, 100, 2, false, false, (float []){0.55,0.675,0.75,0.975}, (float []){70,0.3}, (float []){0.1,0.59}, 0.4);
+  seltext[0] = "#mu p_{T}>16 GeV, |#eta|<2.5";
+  seltext[1] = "N#mu#geq2";
+  //makeratehist("recomubasicsel", "pt2", 10, 100, 2, false, false, (float []){0.55,0.675,0.75,0.975}, (float []){60,12}, (float []){0.1,31}, 22.0);
+
   return -1;
 }
