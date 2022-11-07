c2e3022573a29ba6b6b81efe820d8e6bc85b0185
A.R.Sahasransu <a.r.sahasransu@gmail.com>
2022-11-07 10:17:34 +0100

    Fix branch incompatible type

--- FromNanoAOD/EgEg/robustanalyzer.hh
+++ FromNanoAOD/EgEg/robustanalyzer.hh
@@ -25,10 +25,10 @@ class robustanalyzer {
 
   TFile* outfile;
 
-  int run, lumi, bunch;
+  UInt_t run, lumi, bunch;
   Bool_t HLT_DiPhoton10sminlt0p12, HLT_DiPhoton10Time1p4ns, HLT_PFMET120_PFMHT120_IDTight, HLT_PFMETNoMu110_PFMHTNoMu110_IDTight_FilterHF, HLT_PFMETNoMu120_PFMHTNoMu120_IDTight, HLT_CaloMET80_NotCleaned, HLT_PFMET200_NotCleaned, HLT_PFMET200_BeamHaloCleaned, HLT_MonoCentralPFJet80_PFMETNoMu120_PFMHTNoMu120_IDTight;
   Float_t eventRho;
-  long event;
+  ULong64_t event;
   UInt_t eln;
   Float_t elpt[100], eleta[100], elphi[100], eldetasc[100], eldxy[100], eldxyerr[100], eldz[100], eldzerr[100], elooemoop[100], elhoe[100], elsieie[100], elconvveto[100];
   UInt_t lowpteln;
