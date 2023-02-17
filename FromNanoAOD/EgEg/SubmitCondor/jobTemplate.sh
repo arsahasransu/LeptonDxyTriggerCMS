f4c6b8c712fac27be1d6a3c053b8bf4f3d82ba7d
arsahasransu <a.r.sahasransu@gmail.com>
2023-02-17 08:08:26 +0100

    Update to the submit condor

--- FromNanoAOD/EgEg/SubmitCondor/jobTemplate.sh
+++ FromNanoAOD/EgEg/SubmitCondor/jobTemplate.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
 mkdir Job_$1_$2
-#echo "============ Created the master run folder ============"
+ls
+echo "============ Created the master run folder ============"
 
 cd Job_$1_$2
-echo $TMPDIR
+THISDIR=$(pwd)
+echo $THISDIR
 
-cd /afs/cern.ch/work/a/asahasra/private/Run3DataWithNewTriggers/CMSSW_12_4_10_patch1/src/
+cd /user/asahasra/MeasureTriggerEff/CMSSW_12_6_3/src/
 eval `scramv1 runtime -sh`
 
-cd $TMPDIR/../Job_$1_$2
+cd $THISDIR
 echo $CMSSW_BASE
+echo $1
+echo $2
+echo $3
 
-cp /afs/cern.ch/work/a/asahasra/private/Run3DataWithNewTriggers/CMSSW_12_4_10_patch1/src/LeptonDxyTriggerCMS/FromNanoAOD/EgEg/robustanalyzer* ./
+cp /user/asahasra/MeasureTriggerEff/CMSSW_12_6_3/src/LeptonDxyTriggerCMS/FromNanoAOD/EgEg/robustanalyzer* ./
 
 echo "============ Copied the cpp run files file. Listing directory contents ============"
 ls
@@ -24,13 +29,10 @@ g++ robustanalyzermain.C robustanalyzer.C `root-config --cflags --glibs` -o robu
 
 echo "Compiling successful. Begin execution."
 
-./robustanalyzer.out $2 /afs/cern.ch/work/a/asahasra/private/Run3DataWithNewTriggers/CMSSW_12_4_10_patch1/src/01d4ffc5-02d3-4f41-9afb-7bb4db04a7c7.root hists_metdata_01d4ffc5_$1_ 150
-#./robustanalyzer.out $2 /afs/cern.ch/work/a/asahasra/private/Run3DataWithNewTriggers/CMSSW_12_4_10_patch1/src/b4418752-9427-495f-bea3-22884abb31d0.root hists_metdata_b4418752_$1_ 150
-#./robustanalyzer.out $2 /afs/cern.ch/work/a/asahasra/private/Run3DataWithNewTriggers/CMSSW_12_4_10_patch1/src/7f0e7b28-799d-487d-bdb0-348443cb4987.root hists_metdata_7f0e7b28_$1_ 150
-#./robustanalyzer.out $2 /afs/cern.ch/work/a/asahasra/private/Run3DataWithNewTriggers/CMSSW_12_4_10_patch1/src/890e99ce-7d46-486e-9b4b-8866a3151496.root hists_metdata_890e99ce_$1_ 150
+./robustanalyzer.out $(( $2%20 )) 19 hists_metdata_$1_$2_ $3
 
 echo ""
 echo "Run over. Copy the output files."
 ls
 
-cp hists_metdata_*_$1_$2.root $CMSSW_BASE/src/LeptonDxyTriggerCMS/FromNanoAOD/EgEg/
+cp hists_metdata_$1_$2_*.root $CMSSW_BASE/src/LeptonDxyTriggerCMS/FromNanoAOD/EgEg/
