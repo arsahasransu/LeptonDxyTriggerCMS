044b8869a390f7136f5c2176b707cd3224564af4
A.R.Sahasransu <a.r.sahasransu@gmail.com>
2023-04-24 10:36:48 +0200

    Plotter update

--- MuMu/newplotter.C
+++ MuMu/newplotter.C
@@ -73,10 +73,9 @@ int comparesamevariable(std::vector<TFile*> file, std::vector<TString> cutname, 
 
     if(dividebybinw) {
       for(unsigned int bincnt=1; bincnt<=allhists[histctr]->GetNbinsX(); bincnt++) {
-	allhists[histctr]->SetBinContent(bincnt, allhists[histctr]->GetBinContent(bincnt)/allhists[histctr]->GetBinWidth(bincnt));
+	allhists[histctr]->SetBinContent(bincnt, allhists[histctr]->GetBinContent(bincnt)*1.0/allhists[histctr]->GetBinWidth(bincnt));
       }
     }
-    
     allhists[histctr]->GetXaxis()->SetTitle(xtitle);
     if(normalize) allhists[histctr]->GetYaxis()->SetTitle(yaxistitle);
     else allhists[histctr]->GetYaxis()->SetTitle(yaxistitle);
@@ -332,6 +331,31 @@ int crossChecktwohist(std::vector<TFile*> file, std::vector<TString> cutname, TS
   return -1;
 }
 
+void standalone_plots() {
+
+  TH2F* dataplot = (TH2F*) datafile->Get("sel2recomu_all_log10_dxy_dxysig");
+  dataplot = (TH2F*) dataplot->Clone("sel2recomu_all_log10_dxy_dxysig");
+  TH2F* sig3cmplot = (TH2F*) sig3cmfile->Get("sel2recomu_all_log10_dxy_dxysig");
+  sig3cmplot = (TH2F*) sig3cmplot->Clone("sel2recomu_all_log10_dxy_dxysig");
+  TH2F* sig30cmplot = (TH2F*) sig30cmfile->Get("sel2recomu_all_log10_dxy_dxysig");
+  sig30cmplot = (TH2F*) sig30cmplot->Clone("sel2recomu_all_log10_dxy_dxysig");
+  TH2F* sig1mplot = (TH2F*) sig1mfile->Get("sel2recomu_all_log10_dxy_dxysig");
+  sig1mplot = (TH2F*) sig1mplot->Clone("sel2recomu_all_log10_dxy_dxysig");
+  TH2F* sig3mplot = (TH2F*) sig3mfile->Get("sel2recomu_all_log10_dxy_dxysig");
+  sig3mplot = (TH2F*) sig3mplot->Clone("sel2recomu_all_log10_dxy_dxysig");
+
+  dataplot->SetLineColor(kBlack);
+  sig3cmplot->SetLineColor(kRed+2);
+  sig30cmplot->SetLineColor(kRed);
+  
+  TCanvas* c1;
+  c1 = new TCanvas();
+  dataplot->Draw("BOX");
+  sig3cmplot->Draw("BOX SAME");
+  sig30cmplot->Draw("BOX SAME");
+  c1->SaveAs("./dirplots/Efmrl_sel2recomu_/sel2recomu_all_log10_dxy_dxysig.png");
+}
+
 int newplotter() {
 
   std::vector<int> coloroptrate{kBlack, kRed+3, kRed, kOrange+2, kOrange};
@@ -564,6 +588,13 @@ int newplotter() {
   vector<double> mu_binslog10dxy{-4.0, -3.6, -3.2, -2.8, -2.4, -2.0, -1.6, -1.2, -0.8, -0.4, 0.0, 0.4, 0.8, 1.2, 1.6, 2.0};
   int mu_nbinslog10dxy = mu_binslog10dxy.size()-1;
   comparesamevariable(file, name, "subleadlog10dxy", mu_nbinslog10dxy, &mu_binslog10dxy[0], false, false, false, (float []){0,0.52}, (float []){0.55,0.68,0.825,0.99}, (float []){-3.6, 0.35}, true, "#mu_{2} log_{10}d_{0} [log_{10} cm]", "normalized events / 0.4 log_{10} cm", false, "sel2recomu_subleadmu_log10dxy");
+  vector<double> subleadmupt_binslog10dxy{-3.6, -3.5, -3.4, -3.3, -3.2, -3.1, -3.0, -2.9, -2.8, -2.7, -2.6, -2.5, -2.4, -2.3, -2.2, -2.1, -2.0, -1.9, -1.8, -1.7, -1.6, -1.5, -1.4, -1.3, -1.2, -1.1, -1.0, -0.9, -0.8, -0.7, -0.6, -0.4, -0.2, 0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0};
+  int subleadmupt_nbinslog10dxy = subleadmupt_binslog10dxy.size()-1;
+  comparesamevariable(file, name, "alllog10dxy", subleadmupt_nbinslog10dxy, &subleadmupt_binslog10dxy[0], false, false, false, (float []){0,0.19}, (float []){0.15,0.65,0.4,0.95}, (float []){0,0.15}, true, "#mu_{2} log_{10}d_{0} [log_{10} cm]", "normalized events / log_{10} cm", true, "sel2_alllog10dxy");
+  vector<double> subleadmupt_binslog10dxysig{-1.2, -1.1, -1.0, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0};
+  int subleadmupt_nbinslog10dxysig = subleadmupt_binslog10dxysig.size()-1;
+  comparesamevariable(file, name, "alllog10dxysig", subleadmupt_nbinslog10dxysig, &subleadmupt_binslog10dxysig[0], false, false, false, (float []){0,0.17}, (float []){0.15,0.65,0.4,0.95}, (float []){0,0.4}, true, "#mu_{2} log_{10}(d_{0} sig.) [log_{10} unit]", "normalized events / log_{10} unit", true, "sel2_alllog10dxysig");
+
 
   seltext[0] = "N#mu#geq2, p_{T}#geq16 GeV";
   seltext[1] = "#mu |#eta|<2.5, |d_{0}|>0.01 cm";
@@ -633,10 +664,13 @@ int newplotter() {
   vector<double> mu_binslog10dxysig{-0.6,-0.2,0.2,0.6,1,1.4,1.8,2.2,2.6,3,3.4,3.8,4.2};
   int mu_nbinslog10dxysig = mu_binslog10dxysig.size()-1;
   comparesamevariable(file, name, "subleadlog10dxysig", mu_nbinslog10dxysig, &mu_binslog10dxysig[0], false, false, false, (float []){0,0.52}, (float []){0.125,0.65,0.375,0.99}, (float []){2.2,0.425}, true, "#mu_{2} log_{10}d_{0} sig. [TODO]", "normalized events / 0.4 units", false, "sel31recomu_subleadmu_log10dxysig");
+  comparesamevariable(file, name, "alllog10dxy", subleadmupt_nbinslog10dxy, &subleadmupt_binslog10dxy[0], true, false, false, (float []){5e-3,0.9}, (float []){0.11,0.7,0.36,0.99}, (float []){33,2}, true, "#mu_{2} log_{10}d_{0} / log_{10}cm", "normalized events / 0.2 log_{10}cm", false, "sel31_alllog10dxy");
 
   seltext[0] = "N#mu#geq2, p_{T}#geq16 GeV, |#eta|<2.5";
   seltext[1] = "d_{0}>0.01 cm, d_{0} sig.>1.4";
   makeratehist({"sel30recomu","sel30recomu","sel30recomu","sel30recomu","sel30recomu"}, "subleadpt", subleadmupt_nbinspt, &subleadmupt_binspt[0], false, false, (float []){0.5,0.66,0.7,0.99}, (float []){37,0.4}, (float []){0,1.39}, 0.875, "#mu_{2} p_{T} [GeV]");
 
+  standalone_plots();
+  
   return -1;
 }
